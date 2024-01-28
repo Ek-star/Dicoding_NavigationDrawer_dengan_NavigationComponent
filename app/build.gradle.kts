6d1612530f89fba52646e29a2431beb128dca8c9
Ekalma Toto Alloy Sembiring <totoekalma@gmail.com>
2024-01-28 16:01:06 +0700

    Mengatur gradle (bagian implementation)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,10 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
